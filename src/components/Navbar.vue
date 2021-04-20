fe3b0d17b36bad807dec0dc6b9ed7ca67cdb12b3
WinnieZero <57484936+WinnieZero@users.noreply.github.com>
2021-04-20 17:29:58 +0700

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- Nav bar -->
+    <header
+      class="fixed flex flex-col w-full bg-white sm:relative pin-t pin-r pin-l"
+    >
+      <nav
+        class="flex flex-col items-center justify-between w-full px-4 py-1 bg-blue-900 border-b-4 border-teal-500 shadow sm:flex-row sm:px-6 sm:shadow-none"
+      >
+        <div class="flex flex-row flex-no-wrap items-center self-start justify-between w-full sm:w-auto sm:self-center sm:flex-none"></div>
+        <!-- <a class="py-1 no-underline"
+          ><h1 class="text-lg font-bold text">Get Start</h1></a
+        > -->
+        <ul class="flex-col">
+          <li class="nav-item">
+            <router-link
+              to="/"
+              class="w-full py-2 text-lg font-bold no-underline text-white hover:text-red sm:w-auto sm:pr-4 sm:py-1 sm:pt-2"
+              >New Account</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link
+              to="/about"
+              class="w-full py-2 text-lg font-bold no-underline text-white hover:text-red sm:w-auto sm:pr-4 sm:py-1 sm:pt-2"
+              >account lists</router-link
+            >
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <!-- Router view -->
+    <div class="mt-5 containew">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,143 @@
+<template>
+  <nav-bar :navber="navbar" />
+  <div class="flex items-center min-h-full bg-blue-200 dark:bg-gray-900">
+    <div class="container mx-auto">
+      <div class="max-w-md p-5 mx-auto my-10 bg-white rounded-md shadow-sm">
+        <div class="text-center">
+          <!-- Content goes here -->
+          <h1
+            class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200"
+          >
+            Create Account
+          </h1>
+          <div class="m-7">
+            <form @submit="SubmitUser()">
+              <div class="mb-6">
+                <label
+                  for="firstname"
+                  class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                  >FirstName</label
+                >
+                <input
+                  v-model="user.fullname"
+                  type="text"
+                  name="firstname"
+                  id="firstname"
+                  placeholder="FirstName"
+                  required
+                  class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                />
+              </div>
+              <div class="mb-6">
+                <label
+                  for="lastname"
+                  class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                  >Lastname</label
+                >
+                <input
+                  v-model="user.lastname"
+                  type="text"
+                  name="lastname"
+                  id="lastname"
+                  placeholder="Lastname"
+                  required
+                  class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                />
+              </div>
+              <div class="mb-6">
+                <label
+                  for="email"
+                  class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                  >Email Address</label
+                >
+                <input
+                  v-model="user.email"
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="someone@example.com"
+                  required
+                  class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                />
+              </div>
+              <div class="mb-6">
+                <label
+                  for="phone"
+                  class="text-sm text-gray-600 dark:text-gray-400"
+                  >Phone Number</label
+                >
+                <input
+                  v-model="user.phone"
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  placeholder="0123456789"
+                  required
+                  class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                />
+              </div>
+              <div class="mb-6">
+                <page-button ton="create"></page-button>
+              </div>
+              <p class="text-base text-center text-gray-400" id="result"></p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PageButton from "../components/PageButton.vue";
+
+export default {
+  name: "Home",
+  components: { PageButton },
+  data() {
+    return {
+      user: {
+        firstname: "",
+        lastname: "",
+        email: "",
+        phone: "",
+      },
+      url: "http://localhost:5000/user",
+      result: [],
+    };
+  },
+
+  methods: {
+    SubmitUser() {
+      this.addNewUser({
+        firstname: this.user.firstname,
+        lastname: this.user.lastname,
+        email: this.user.email,
+        phone: this.user.phone,
+      });
+      this.$router.push('/about')
+    },
+
+    async addNewUser(newUser) {
+      try {
+        const res = await fetch(this.url, {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            Firstname: newUser.firstname,
+            Lastname: newUser.lastname,
+            Email: newUser.email,
+            Phone: newUser.phone,
+          }),
+        });
+        const data = await res.json();
+        this.result = [...this.result, data];
+      } catch (error) {
+        console.log(`Colud not save! ${error}`);
+      }
+    },
+  },
+};
+</script>
